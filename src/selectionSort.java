f6cc3a514100cd25306b786ef59f5cf597361437
Santosh Shingare <smshingare@paypal.com>
2022-04-17 18:22:58 +0530

    Insertion sort added

---
+++ src/selectionSort.java
@@ -0,0 +1,28 @@
+public class selectionSort {
+    public static void main(String args[]) {
+        int arr[] = { 7, 8, 1, 3, 2 };
+        // selection sort
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[smallest]) {
+                    smallest = j;
+                }
+            }
+            // swap
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr, "eee");
+    }
+
+    // Print array
+    public static void printArray(int arr[], String mode) {
+        System.out.print("Array " + mode + " bubble sort :");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
